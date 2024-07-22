39da9fe4bef0c338ff85428b51615984ac5a8dc9
Fabian Ramirez <fabianramirez0072@gmail.com>
2024-07-22 17:22:33 -0500

    new endpoints

--- Api/Controllers/AuthenticationController.cs
+++ Api/Controllers/AuthenticationController.cs
@@ -1,6 +1,9 @@
-using Application.UseCase.Security.Authentication.Commands;
+using Application.UseCase.Security.Authentication.Commands.SingIn;
+using Application.UseCase.Security.Authentication.Commands.SignUp;
+using Application.UseCase.Security.Authentication.Commands.SignOut;
 using Microsoft.AspNetCore.Mvc;
 using MediatR;
+using Domain.Exceptions;
 
 namespace Api.Controllers
 {
@@ -9,10 +12,23 @@ namespace Api.Controllers
     public class AuthenticationController(IMediator _mediator) : ControllerBase
     {
 
-        [HttpPost(Name = "SingIn")]
+        [HttpPost("SignIn")]
         public async Task<ActionResult> SingIn(AuthenticationCommand authenticationCommand)
         {
             return Ok(await _mediator.Send(authenticationCommand));
+        }
+
+        [HttpPost("SignUp")]
+        public async Task<ActionResult> SingUp(SignUpCommand signUpCommand)
+        {
+            return Ok(await _mediator.Send(signUpCommand));
+        }
+
+        [HttpPost("SignOut")]
+        public async Task<ActionResult> SingOut()
+        {
+            var sessionId = User.FindFirst("Session")!.Value ?? throw new NoContentException("Session No Defined");
+            return Ok(await _mediator.Send(new SignOutCommand(Guid.Parse(sessionId))));
         }
     }
 }
